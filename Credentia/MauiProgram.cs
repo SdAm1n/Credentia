25a38e088b232805ea32a9c9c415792898afd9d9
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-03-24 02:09:41 +0600

    Modified Login Page

--- Credentia/MauiProgram.cs
+++ Credentia/MauiProgram.cs
@@ -14,8 +14,13 @@ namespace Credentia
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+                    fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+                    fonts.AddFont("Roboto-Light.ttf", "RobotoLight");
+
+
+                    //fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    //fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
 #if DEBUG
